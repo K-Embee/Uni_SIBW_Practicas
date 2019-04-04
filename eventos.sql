2f3fa599c5a4aaf03a0c60dc9bdad1a4f1d06816
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-04-04 10:28:41 +0200

    Base de datos ordenada y organizacion de imagenes

--- eventos.sql
+++ eventos.sql
@@ -2,10 +2,10 @@
 -- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Apr 02, 2019 at 10:38 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 7.3.3
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 04-04-2019 a las 00:54:15
+-- Versión del servidor: 10.1.38-MariaDB
+-- Versión de PHP: 7.3.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,18 +19,28 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `eventos`
+-- Base de datos: `eventos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `comentarios`
+-- Estructura de tabla para la tabla `censura`
+--
+
+CREATE TABLE `censura` (
+  `palabra` varchar(45) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `comentarios`
 --
 
 CREATE TABLE `comentarios` (
   `idComentario` int(11) NOT NULL,
-  `eventoNombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `idEvento` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `ip` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `nombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `correo` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
@@ -41,10 +51,22 @@ CREATE TABLE `comentarios` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `evento`
+-- Estructura de tabla para la tabla `etiquetas`
+--
+
+CREATE TABLE `etiquetas` (
+  `genero` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `idEvento` varchar(100) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `evento`
 --
 
 CREATE TABLE `evento` (
+  `idEvento` varchar(100) COLLATE utf8_spanish_ci NOT NULL,
   `eventoNombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `genero` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
   `estudios` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
@@ -58,51 +80,121 @@ CREATE TABLE `evento` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Dumping data for table `evento`
+-- Volcado de datos para la tabla `evento`
 --
 
-INSERT INTO `evento` (`eventoNombre`, `genero`, `estudios`, `distribuidora`, `fechaEstreno`, `descripcion`, `enlace_twitter`, `enlace_fb`, `fecha_creacion`, `fecha_ultima_mod`) VALUES
-('Detective Pikachu', 'Acción, Aventuras', 'Legendary Pictures', 'Warner Bros. Pictures', '2019-05-17', 'Tim Goodman llega a Ryme City para investigar la misteriosa desaparición de su padre en la ciudad. En el camino,\r\n				 se encuentra con un Pikachu que habla, aunque en realidad solo él es el único que puede entenderlo. Ambos trabajarán\r\n				  juntos para resolver este gran enigma junto a la reportera Kathryn Newton con su Psyduck.', 'https://twitter.com/DetPikachuMovie', 'https://www.facebook.com/detectivepikachumovie/', '2019-04-02', '2019-04-02');
+INSERT INTO `evento` (`idEvento`, `eventoNombre`, `genero`, `estudios`, `distribuidora`, `fechaEstreno`, `descripcion`, `enlace_twitter`, `enlace_fb`, `fecha_creacion`, `fecha_ultima_mod`) VALUES
+('detective_pikachu', 'Detective Pikachu', 'Acción, Aventuras', 'Legendary Pictures', 'Warner Bros. Pictures', '2019-05-17', 'Tim Goodman llega a Ryme City para investigar la misteriosa desaparición de su padre en la ciudad. En el camino,\r\n				 se encuentra con un Pikachu que habla, aunque en realidad solo él es el único que puede entenderlo. Ambos trabajarán\r\n				  juntos para resolver este gran enigma junto a la reportera Kathryn Newton con su Psyduck.', 'https://twitter.com/DetPikachuMovie', 'https://www.facebook.com/detectivepikachumovie/', '2019-04-02', '2019-04-02');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `imagen`
+-- Estructura de tabla para la tabla `genero`
+--
+
+CREATE TABLE `genero` (
+  `genero` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `imagen`
 --
 
 CREATE TABLE `imagen` (
   `url` varchar(300) COLLATE utf8_spanish_ci NOT NULL,
-  `eventoNombre` varchar(200) COLLATE utf8_spanish_ci NOT NULL
+  `idEvento` varchar(100) COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
--- Dumping data for table `imagen`
+-- Volcado de datos para la tabla `imagen`
 --
 
-INSERT INTO `imagen` (`url`, `eventoNombre`) VALUES
-('./imgs/pikachu-inicio.jpg', 'Detective Pikachu');
+INSERT INTO `imagen` (`url`, `idEvento`) VALUES
+('./imgs/detective_pikachu/portada.jpg', 'detective_pikachu');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Estructura de tabla para la tabla `pag_info`
+--
+
+CREATE TABLE `pag_info` (
+  `idPagina` varchar(50) COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
 --
 
 --
--- Indexes for table `comentarios`
+-- Indices de la tabla `censura`
+--
+ALTER TABLE `censura`
+  ADD PRIMARY KEY (`palabra`);
+
+--
+-- Indices de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  ADD PRIMARY KEY (`idComentario`);
+  ADD PRIMARY KEY (`idComentario`),
+  ADD KEY `ClaveExternaEvento` (`idEvento`);
 
 --
--- Indexes for table `evento`
+-- Indices de la tabla `etiquetas`
+--
+ALTER TABLE `etiquetas`
+  ADD PRIMARY KEY (`genero`,`idEvento`),
+  ADD KEY `Clave Externa Evento` (`idEvento`);
+
+--
+-- Indices de la tabla `evento`
 --
 ALTER TABLE `evento`
-  ADD PRIMARY KEY (`eventoNombre`);
+  ADD PRIMARY KEY (`idEvento`);
 
 --
--- Indexes for table `imagen`
+-- Indices de la tabla `genero`
+--
+ALTER TABLE `genero`
+  ADD PRIMARY KEY (`genero`);
+
+--
+-- Indices de la tabla `imagen`
 --
 ALTER TABLE `imagen`
-  ADD PRIMARY KEY (`url`(200));
+  ADD PRIMARY KEY (`url`(200)),
+  ADD KEY `Clave Externa` (`idEvento`);
+
+--
+-- Indices de la tabla `pag_info`
+--
+ALTER TABLE `pag_info`
+  ADD PRIMARY KEY (`idPagina`);
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `comentarios`
+--
+ALTER TABLE `comentarios`
+  ADD CONSTRAINT `ClaveExternaEvento` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`);
+
+--
+-- Filtros para la tabla `etiquetas`
+--
+ALTER TABLE `etiquetas`
+  ADD CONSTRAINT `Clave Externa Evento` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`),
+  ADD CONSTRAINT `Clave Externa Genero` FOREIGN KEY (`genero`) REFERENCES `genero` (`genero`);
+
+--
+-- Filtros para la tabla `imagen`
+--
+ALTER TABLE `imagen`
+  ADD CONSTRAINT `Clave Externa` FOREIGN KEY (`idEvento`) REFERENCES `evento` (`idEvento`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
