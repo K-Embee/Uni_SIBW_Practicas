4b8fcd06ab865e2637d2a2aa0a32fb01dc360a2a
monicordovilla <32360865+monicordovilla@users.noreply.github.com>
2019-04-16 18:26:45 +0200

    Update botones.js

--- scripts/botones.js
+++ scripts/botones.js
@@ -1,16 +1,21 @@
 function botonTwitter(){
     var mensaje;
+    var myWindow = window.open("", "publicar", "width=300,height=200");
     var nombre = document.getElementById("eventoNombre").innerHTML;
     var imagen =document.getElementById("imagenPortada").cloneNode();
+    //mensaje ="Se publicará en twitter el siguiente mensaje:\n " + nombre + "\n"
 
-    var opcion = confirm("Se publicará en twitter el siguiente mensaje:\n " + nombre + "\n");
-    opcion.appendChild(imagen);
-    if (opcion == true) {
+    myWindow.document.write("<p>Se publicará en twitter el siguiente mensaje: </p>");
+    myWindow.document.writeln("<var>" + nombre + "</var>");
+    myWindow.document.write("<button onclick=\"close()\">Aceptar</button>")
+    //var opcion = alert("Se publicará en twitter el siguiente mensaje:\n " + nombre + "\n" + imagen);
+    /*if (opcion == true) {
         mensaje = "Has clickado OK";
 	} else {
 	    mensaje = "Has clickado Cancelar";
-	}
-    document.getElementById("ventanaTwitter").innerHTML = mensaje;
+	}*/
+    //document.getElementById("ventanaTwitter").innerHTML = mensaje;
+
 }
 function botonFacebook(){
 
