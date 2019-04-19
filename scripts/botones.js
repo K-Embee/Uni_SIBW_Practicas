c706e064575ddb883ce647f2708148bea2f062fc
K-Embee <55490375+K-Embee@users.noreply.github.com>
2019-04-19 17:03:27 +0200

    "My eyes" mode

--- scripts/botones.js
+++ scripts/botones.js
@@ -9,3 +9,27 @@ function botonTwitter(idEvento){
 function botonFacebook(idEvento){
     window.open("/?evento="+idEvento+"&fb_tw=fb" , "publicar", "width=400,height=300");
 }
+
+//Una tonteria hecha con localStorage
+
+function misOjos(toggle) {
+    if (window.localStorage !=  null) {
+        var misOjos = JSON.parse(localStorage.getItem('misOjos'));
+        if(toggle){
+            if(misOjos) {
+                localStorage.setItem('misOjos', JSON.stringify(false));
+                misOjos = false;
+            }
+            else {
+                localStorage.setItem('misOjos', JSON.stringify(true));
+                misOjos = true;
+            }
+        }
+        var body =  document.getElementsByTagName("BODY")[0];
+        body.style.filter = ((misOjos)?"invert(1)":"hue-rotate(0)");
+        console.log(body);
+    }
+    else {
+          // No localStorage support
+    }
+}
